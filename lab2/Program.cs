6b3cdfdca67a5c3d93fa2ed62a6d42ca5afec17d
Rohan <rohanprajapati121212@gmail.com>
2025-08-07 12:07:40 +0530

    Lab2 Q2

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -2,6 +2,29 @@
 
 namespace lab2
 {
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Q1. Welcome to the User Profile and Vehicle Management System!");
+            Q1UserProfile user = new Q1UserProfile();
+            user.SetUserDetails();               
+            user.DisplayUserDetails();
+            
+            Console.WriteLine("\nQ2. Welcome to the Vehicle Management System!");
+            Truck truck = new Truck();
+            truck.SetVehicleDetails();
+            truck.SetTruckDetails();
+            truck.DisplayDetails();
+            Bus bus = new Bus();
+            bus.SetVehicleDetails();
+            bus.SetBusDetails();
+            bus.DisplayDetails();
+
+
+        }
+    }
     public class Q1UserProfile
     {
         private string UserName;
@@ -32,14 +55,66 @@ namespace lab2
             Console.WriteLine($"Email: {email}");
         }
     }
-
-    public class Program
+    public class Vehicle
     {
-        public static void Main(string[] args)
+        public string Make;
+        public string Model;
+        public int Year;
+
+        public void SetVehicleDetails()
         {
-            Q1UserProfile user = new Q1UserProfile();
-            user.SetUserDetails();               
-            user.DisplayUserDetails();           
+            Console.Write("\nEnter Vehicle Make: ");
+            Make = Console.ReadLine();
+            Console.Write("Enter Vehicle Model: ");
+            Model = Console.ReadLine();
+
+            Console.Write("Enter Vehicle Year: ");
+            Year=int.Parse(Console.ReadLine());
+            while (Year < 1900 || Year > DateTime.Now.Year)
+            {
+                Console.Write("Invalid year. Enter a valid year (1900 to current): ");
+                Year = int.Parse(Console.ReadLine());
+            }
+        }
+
+    }
+
+    // Derived class: Truck
+    public class Truck : Vehicle
+    {
+        public int LoadCapacity;
+
+        public void SetTruckDetails()
+        {
+            Console.Write("Enter Truck Load Capacity (in tons): ");
+            LoadCapacity = int.Parse(Console.ReadLine());
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("\nTruck Details:");
+            Console.WriteLine($"Make: {Make}, Model: {Model}, Year: {Year}, Load Capacity: {LoadCapacity} tons");
         }
     }
+
+
+    // Derived class: Bus
+    public class Bus : Vehicle
+    {
+        public int SeatingCapacity;
+
+        public void SetBusDetails()
+        {
+            Console.Write("Enter Bus Seating Capacity: ");
+            SeatingCapacity = int.Parse(Console.ReadLine());
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("\nBus Details:");
+            Console.WriteLine($"Make: {Make}, Model: {Model}, Year: {Year}, Seating Capacity: {SeatingCapacity} passengers");
+        }
+    }
+
 }
+
