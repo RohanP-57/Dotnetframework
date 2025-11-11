eb20a38a355419cfd1cb731e43a7117bf519ce95
Rohan <rohanprajapati121212@gmail.com>
2025-11-11 20:07:28 +0530

    Class Library
    Successfully Implemented Class library and forms DotNetframework

---
+++ FormApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using class_library;
+namespace FormApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(textBox1.Text);
+                int b = int.Parse(textBox2.Text);
+                int result = new Class1().sum(a, b);
+                label1.Text = result.ToString();
+            }
+            catch {
+                label1.Text = "Error";
+            }
+        }
+    }
+}
---
+++ class_library/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_library
+{
+    public class Class1
+    {
+        public int sum(int a, int b ) { return a + b; }
+    }
+}
